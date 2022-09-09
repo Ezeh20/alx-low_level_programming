c9dbefda5a70a12459f141082e18212e52fe1501
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-08 19:51:27 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/*betty*/
+/**
+ * main - Entry point
+ *
+ * Return always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
