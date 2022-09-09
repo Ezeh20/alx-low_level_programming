62786b63f8cc90be80e22570efe6ee8405f187e3
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-09 15:51:45 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * REturn: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char c;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
