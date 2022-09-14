e3cab05f0a7fec1d1da3bacf03a4dc317589766e
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-13 20:03:56 -0700

    ..

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase
+ * _islower - checks for lowercase character
  * @c: the character to check
  * Return 1 if c is lower, 0 otherwise
  */
