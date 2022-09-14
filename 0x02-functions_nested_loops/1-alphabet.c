96c7ebe45bb83da2c6e0ecddcef306935f9c5aa4
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-13 18:59:18 -0700

    a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * 
  *
- * Return: Always 0 (Success)
+ *
  */
 
 void print_alphabet(void)
@@ -20,4 +20,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-
