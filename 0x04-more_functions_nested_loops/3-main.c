09ba963ac44001e10fc5e505a6e4e5018e3dcd9a
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-15 14:37:00 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void);
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
