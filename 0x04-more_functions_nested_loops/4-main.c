aef85f0d2dbf4b8d0a60f5628a6d9703f62c226c
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-15 14:49:08 -0700

    print numbers from 0 to 9  except 2 && 4

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void);
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
