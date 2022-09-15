94a9ce8dd5cfec6a40b7fa25343be8dc15a2bbaa
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-15 16:42:34 -0700

    py

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+void print_triangle(int size);
+
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	return (0);
+}
