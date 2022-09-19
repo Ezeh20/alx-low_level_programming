3437fc207163dcf57cf78939411f37f0dee439b5
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-19 15:58:41 -0700

    swap variable values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap - this functions swaps the value of each int a and b
+ * where a == b and b == a
+ * @a: first int
+ * @b: second int
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
