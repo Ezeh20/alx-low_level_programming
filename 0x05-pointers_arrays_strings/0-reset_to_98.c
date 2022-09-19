0262bcd4f1b5dfc07076fbb8b188d975ff967a82
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-19 15:41:21 -0700

    update a variable using dereferencing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98: this updates the value of n to a new value (98)
+ * reset_to_98 - this updates the value of n to a new value (98)
  * @n: the variable to update
  */
 
