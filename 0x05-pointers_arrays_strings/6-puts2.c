b436e2ebc327f5ffaea380c85ab999583476f146
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-19 17:09:54 -0700

    even numbers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,19 +5,15 @@
  *  @str: string to print the chars from
  */
 
-
 void puts2(char *str)
 {
 	int i;
 	int j;
 
 	i = 0;
-
 	while (str[i] != '\0')
 		i++;
-
 	for (j = 0; j < i; j += 2)
 		_putchar(str[j]);
 	_putchar('\n');
 }
-
