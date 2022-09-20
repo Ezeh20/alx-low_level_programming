f35ceebbd5ca95c5a289ccc96f1147a0c9332f05
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-19 17:08:33 -0700

    even numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string, starting with the first character, followed by a new line.
+ *  @str: string to print the chars from
+ */
+
+
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	for (j = 0; j < i; j += 2)
+		_putchar(str[j]);
+	_putchar('\n');
+}
+
