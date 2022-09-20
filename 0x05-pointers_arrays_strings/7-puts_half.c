c3e43720a02ba562542cf9efffb1400be20cfb87
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-09-19 17:26:59 -0700

    half the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the string
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	if (1 % 2 == 0)
+		for (k = i / 2;  str[k] != '\0'; k++)
+			_putchar(str[i]);
+	else if (i % 2)
+		for (j = (i - 1) / 2; j < i - 1; j++)
+			_putchar(str[j + 1]);
+	_putchar('\n');
+}
