7e6273f14ce228603d2ad7d3f7456dd3776904f8
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-10-10 14:53:35 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+
+
+/**
+ * struct dog - a dog {object} that hold diffrent data types
+ * @name: name (first entry)
+ * @age: age (second entry)
+ * @owner: owner (last entry)
+ *
+ * Description: des
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif
